90020840bac879657c77842c008ca832d132c532
Factories-git <official.factories@gmail.com>
2024-11-09 10:39:41 +0900

    Changes

--- 이중우선순위큐.py
+++ 이중우선순위큐.py
@@ -1,4 +1,4 @@
-import heapq,sys
+import heapq, sys
 
 input = sys.stdin.readline
 print = sys.stdout.write
@@ -8,42 +8,35 @@ max_pq = []
 heapq.heapify(min_pq)
 heapq.heapify(max_pq)
 
-def pop(heap,deletes):
-    back = []
+
+def pop(heap, deletes):
     while heap:
-        s = heapq.heappop(heap)
-        if s not in deletes:
-            heapq.heappush(back, s)
-        else:
+        v, n = heapq.heappop(heap)
+        if n == deletes:
             break
-    heap = back
     return heap
 
 
-
-def Dual_Prioirity_queue(max_heap,min_heap, operations, deletes):
-    for i in operations:
-        if i[0] == 'I':
-            heapq.heappush(min_heap, int(i[1]))
-            heapq.heappush(max_heap, -int(i[1]))
-        elif i[0] == 'D':
+def Dual_Prioirity_queue(max_heap, min_heap, operations, deletes):
+    for k in range(len(operations)):
+        if operations[k][0] == 'I':
+            heapq.heappush(min_heap, (int(operations[k][1]), k))
+            heapq.heappush(max_heap, (-int(operations[k][1]), k))
+        elif operations[k][0] == 'D':
             if min_heap and max_heap:
-                if int(i[1]) < 0:
-                    d = heapq.heappop(min_heap)
-                    deletes.add(d)
+                if int(operations[k][1]) < 0:
+                    d,deletes = heapq.heappop(min_heap)
                     max_heap = pop(max_heap, deletes)
                 else:
-                    d = -heapq.heappop(max_heap)
-                    deletes.add(d)
+                    d, deletes = heapq.heappop(max_heap)
                     min_heap = pop(min_heap, deletes)
 
-        deletes = set()
-    return f'{-max_heap[0]} {min_heap[0]}' if max_heap and min_heap else 'EMPTY'
+    return f'{max_heap} {min_heap}' if max_heap and min_heap else 'EMPTY'
 
 
 for _ in range(int(input())):
     operation = []
     delete = set()
     for i in range(int(input())):
-        operation.append(list(map(str ,input().split())))
+        operation.append(list(map(str, input().split())))
     print(f'{Dual_Prioirity_queue(max_pq, min_pq, operation, delete)}\n')
