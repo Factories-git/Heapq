4a38d1c67c6e72968424f65a96647f3daf46d426
Factories-git <official.factories@gmail.com>
2024-11-09 19:29:25 +0900

    Changes

--- 이중우선순위큐.py
+++ 이중우선순위큐.py
@@ -1,42 +1,51 @@
-import heapq, sys
+import heapq
+buffer = []
+heapq.heapify(buffer)
 
-input = sys.stdin.readline
-print = sys.stdout.write
+
+#
+def synchronize(heap):  # 데이터 힙과 삭제 버퍼 동기화
+    while (heap and buffer) and heap[0] == buffer[0]:
+        heapq.heappop(heap)
+        heapq.heappop(buffer)
+
+
+def erase(heap, x):  # 임의의 값 삭제
+    heapq.heappush(buffer, x*-1)
+    synchronize(heap)
+
+
+def pop(heap):  # 최솟값 조회 및 삭제
+    x = heapq.heappop(heap)
+    synchronize(heap)
+    return x
+
+
+def DualPriortyQueue(operations, min_heap, max_heap):
+    for oper in range(len(operations)):
+        if operations[oper][0] == 'I':
+            heapq.heappush(min_heap, int(operations[oper][1]))
+            heapq.heappush(max_heap, -int(operations[oper][1]))
+        else:
+            if max_heap and min_heap:
+                if int(operations[oper][1]) == 1:
+                    erase(min_heap, pop(max_heap))
+                    synchronize(min_heap)
+                else:
+                    erase(max_heap, pop(min_heap))
+                    synchronize(max_heap)
+
+    return f'{-max_heap[0]} {min_heap[0]}' if max_heap and min_heap else 'EMPTY'
+
 
 min_pq = []
 max_pq = []
 heapq.heapify(min_pq)
 heapq.heapify(max_pq)
-
-
-def pop(heap, deletes):
-    while heap:
-        v, n = heapq.heappop(heap)
-        if n == deletes:
-            break
-    return heap
-
-
-def Dual_Prioirity_queue(max_heap, min_heap, operations, deletes):
-    for k in range(len(operations)):
-        if operations[k][0] == 'I':
-            heapq.heappush(min_heap, (int(operations[k][1]), k))
-            heapq.heappush(max_heap, (-int(operations[k][1]), k))
-        elif operations[k][0] == 'D':
-            if min_heap and max_heap:
-                if int(operations[k][1]) < 0:
-                    d,deletes = heapq.heappop(min_heap)
-                    max_heap = pop(max_heap, deletes)
-                else:
-                    d, deletes = heapq.heappop(max_heap)
-                    min_heap = pop(min_heap, deletes)
-
-    return f'{max_heap} {min_heap}' if max_heap and min_heap else 'EMPTY'
-
-
 for _ in range(int(input())):
     operation = []
-    delete = set()
+    buffer = []
     for i in range(int(input())):
         operation.append(list(map(str, input().split())))
-    print(f'{Dual_Prioirity_queue(max_pq, min_pq, operation, delete)}\n')
+    print(f'{DualPriortyQueue(operation, min_pq, max_pq)}')
+
