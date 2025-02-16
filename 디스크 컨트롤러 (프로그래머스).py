1bf2ab60bf5303870f7a81a6f19f149d91da83dc
Factories-git <official.factories@gmail.com>
2025-02-16 10:08:28 +0900

    Changes

---
+++ 디스크 컨트롤러 (프로그래머스).py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(jobs):
+    answer = 0
+    rng = [i[1] for i in jobs]
+    queue = []
+    time_work = {i:[] for i in range(max(rng))}
+
+    for s, e in jobs:
+        time_work[s].append([s, e])
+    print(time_work)
+    for i in range(sum(rng)):
+        pass
+    return answer
+
+print(solution(	[[7, 8], [3, 5], [9, 6]]))
