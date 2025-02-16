631c7fe28004a1a958d3ec9749ab541f1ba38e7f
Factories-git <official.factories@gmail.com>
2025-02-16 20:34:47 +0900

    Changes

--- 디스크 컨트롤러 (프로그래머스).py
+++ 디스크 컨트롤러 (프로그래머스).py
@@ -1,17 +1,38 @@
 import heapq
-#미완성 버전
 
 def solution(jobs):
     answer = 0
-    rng = [i[1] for i in jobs]
     queue = []
-    time_work = {i:[] for i in range(max(rng))}
+    now = []
+    time_work = {i[0]:[] for i in jobs}
 
-    for s, e in jobs:
-        time_work[s].append([s, e])
-    print(time_work)
-    for i in range(sum(rng)):
-        pass
-    return answer
+    for idx, i in enumerate(jobs):
+        s, e = i[0], i[1]
+        time_work[s].append([e, s, idx])
+    ms = 0
+    c = 0
+    last = 0
+    while True:
+        if not time_work.get(ms) is None:
+            for i in range(len(time_work[ms])):
+                ms_work = time_work[ms][i]
+                heapq.heappush(queue, (ms_work[0], ms_work[1], ms_work[-1]))
+        if now:
+            if now[0]+last == ms:
+                answer += (now[0] + last) - now[1]
+                now = []
+                last = ms
+                c += 1
+        if queue:
+            if not now:
+                now = heapq.heappop(queue)
+
+        if not now and len(jobs) == c:
+            break
+        ms += 1
+        if not now:
+            last += 1
+    return answer//len(jobs)
+
 
 print(solution(	[[7, 8], [3, 5], [9, 6]]))
